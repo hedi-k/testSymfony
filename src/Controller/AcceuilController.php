98ad39ae156089104eebd6a5c37a32dad463260d
hedi <idehk@hotmail.fr>
2023-11-14 15:25:16 +0100

    "Phase 3 : utilisation de Twig

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -3,26 +3,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Explication des bases :
- * La fonction index() retourne une Response. Pour se faire il faut ajouer le use Response.
- * Cette Response est un simple hello world dans ce cas.
- * Pour appeler cette fonction, on fait un commentaire normalisé /** enter 
- * Dans le commentaire @Route("/", name="acceuil")
- * l'annotation pour préciser que cette fonction est exécutée avec la route "/" qui correspond à l'url de base 
- *du site et à laquelle on donnera le nom "accueil" (ce qui permettra d'y accéder aussi par son nom) 
- *Et pour finir on appel le bon use (annotation Route) pour pouvoir utiliser @Route
+ * Ajouter  extends AbstractController permet d'utiliser les fichiers twig
+ * Pour utiliser une propriété ou méthode dans une méthode en php
+ *      $this->unePropriété = ...;
+ *      $this->uneAutreMethode(...);
+ * La méthode render (qui est retourné a l'appel de la fonction index)permet de 
+ * Faire une requête sur une URI (donc renvoyer un chemin) 
  * @author 1050
  */
-class AcceuilController {
+class AcceuilController extends AbstractController {
     /**
      * @Route("/", name="acceuil")
      * @return Response
      */
     public function index(): Response{
-        return new Response ('hello world >:)');
+        return $this->render("pages/acceuil.html.twig");
     }
 }
